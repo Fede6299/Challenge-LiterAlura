8adac6cc6b21672d2ecbba564a7ba53bc30962f2
Fede6299 <fapaza@frba.utn.edu.ar>
2025-01-15 11:41:00 -0300

    feat: consumo de API y conversión de datos
    - Consumo de datos desde la API Gutendex utilizando ApiConsumer.
    - Conversión de datos JSON a la clase Data con JsonDataConverter.
    - Despliegue de resultados (primeros 5 libros) para probar la funcionalidad.

--- src/main/java/com/alura/literalura/main/Main.java
+++ src/main/java/com/alura/literalura/main/Main.java
@@ -1,14 +1,24 @@
 package com.alura.literalura.main;
 
+import com.alura.literalura.model.Data;
 import com.alura.literalura.service.ApiConsumer;
+import com.alura.literalura.service.JsonDataConverter;
 
 public class Main {
     private final ApiConsumer apiConsumer = new ApiConsumer();
-    private final String URL_BASE = "https://gutendex.com/books/?search=pride";
+    private final String URL_BASE = "https://gutendex.com/books/";
+    private final JsonDataConverter jsonDataConverter = new JsonDataConverter();
 
     public void menu(){
+//        Datos generales de los libros
         var json = apiConsumer.getDataFromUrl(URL_BASE);
         System.out.println("Test data from API:");
         System.out.println(json);
+//        Datos de los libros convertidos en Data.class
+        var data = jsonDataConverter.convertJsonToClass(json, Data.class);
+        System.out.println("Data converted to class for the first 5 books:");
+        data.results().stream()
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
